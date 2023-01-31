c0ee717aa8edffdf40b522b7c2f20d4a5ed8fa2c
José Luis Alcaraz <joseluis2958@gmail.com>
2023-01-31 12:46:49 +0100

    hecho ejercicio3

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,11 +1,13 @@
-let num=[], par=0, impar=0, suma=0, mayor=0, menor=0
+let num=[], par=0, impar=0, suma=0, mayor=0, menor=0, i=0
 
-num[0]=parseInt(prompt("Dame un número >= 1"));
-
-for(let i=1;num[i-1]>=1;i++){
+do{
 	num[i]=parseInt(prompt("Dame un número >= 1"));
   if(num[i]>=1){
   	suma+=num[i]
+    
+    if(i==0){
+    	menor=num[0]
+    }
     
   	if(num[i]%2==0){
     	par++
@@ -22,10 +24,13 @@ for(let i=1;num[i-1]>=1;i++){
   }else{
   	num.pop()
   }
-}
+  i++
+}while(num[i-1]>=1)
 
 console.log("Lista de números introducidos: " + num)
 console.log("Número Pares: " + par)
 console.log("Número Impares: " + impar)
 console.log("Suma total: " + suma)
 console.log("Media: " + suma/num.length)
+console.log("Mayor: " + mayor)
+console.log("Menor: " + menor)

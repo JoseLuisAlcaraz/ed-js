1d5855b1ee83fd0ca014937e939ea2a1a4770f02
José Luis Alcaraz <joseluis2958@gmail.com>
2023-02-02 12:58:17 +0100

    Update ejercicio1.js

--- ejercicio1.js
+++ ejercicio1.js
@@ -11,12 +11,20 @@ console.log("Patrones a buscar : " + p.toString())
 console.log("Aparecen : " + totalPatrones(ss, p))
 
 function encontrarPatron(cadena,patron){
-	
+	let cont=0, i=-1
+	do{
+		i=cadena.indexOf(patron,i+1)
+    if(i!==-1){
+    	cont++
+    }
+  }while(i!=-1)
+  return cont
 }
 
 function totalPatrones(cadena, arrayPatrones){
-let sum=0;
+	let sum=0;
 	for(let i=0;i<arrayPatrones.length;i++){
   	sum+=encontrarPatron(cadena,arrayPatrones[i])
   }
+  return sum
 }
